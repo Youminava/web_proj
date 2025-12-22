9f1e47d01fd0620215e8361f86b06c162546406d
Youminava <kirill.2301@bk.ru>
2025-12-22 23:16:39 +0300

    update base and name for web_proj repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: mode === 'production' ? '/drupal/' : '/',
+    base: mode === 'production' ? '/web_proj/' : '/',
     build: {
         outDir: 'build',
     },
