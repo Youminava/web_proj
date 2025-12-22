8cc1880f062d512b6cc09a1a75e0a7d03c3a0b5d
Youminava <kirill.2301@bk.ru>
2025-12-23 02:46:44 +0300

    fix кнопки 'Наши работы'

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,7 +1,7 @@
 export const mainNavItems = [
     { id: 'support', href: '#support', label: 'Поддержка сайтов' },
     { id: 'tariffs', href: '#tariffs', label: 'Тарифы' },
-    { id: 'cases', href: '#cases', label: 'Последние кейсы' },
+    { id: 'cases', href: '#cases', label: 'НАШИ РАБОТЫ' },
     { id: 'reviews', href: '#reviews', label: 'Отзывы' },
     { id: 'contacts', href: '#contacts', label: 'Контакты' },
 ]
